e9860511b93d79a9d264b8aab74f7720f07ac0b0
aroxu <30624945+aroxu@users.noreply.github.com>
2021-07-27 20:09:58 +0900

    Update Intro.tsx

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -8,7 +8,9 @@ const Intro = () => (
         <h2 className='Intro-header text-white font-bold'>
           이상과 상상, 그 이상
         </h2>
-        <p className='Intro-desc text-white'>흥미로운것을 상상하고 만듭니다.</p>
+        <p className='Intro-desc text-white'>
+          흥미로운 것을 상상하고 만듭니다.
+        </p>
       </div>
       <div className='Arrow text-white'>↓</div>
     </div>
