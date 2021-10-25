6274959d711cd3c6c37ab78d2a6264d99416be54
aroxu <30624945+aroxu@users.noreply.github.com>
2021-10-25 12:38:22 +0900

    :broom: 서브 이메일 제거

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -19,14 +19,6 @@ const Contact = () => (
                 target='_blank'
                 rel='noopener noreferrer'>
                 여기
-              </a>{' '}
-              또는{' '}
-              <a
-                className='Link font-bold'
-                href='mailto:zeus_02@naver.com'
-                target='_blank'
-                rel='noopener noreferrer'>
-                여기
               </a>
               를 눌러서 저에게 이메일을 보낼 수 있습니다.
             </span>
